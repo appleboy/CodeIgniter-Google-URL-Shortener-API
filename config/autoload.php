56707229132766e9362ff8e922538f405eefd193
Bo-Yi Wu <appleboy.tw@gmail.com>
2011-10-25 19:58:31 +0800

    Fix autoload error

--- config/autoload.php
+++ config/autoload.php
@@ -4,4 +4,4 @@
 $autoload['config'] = array('google_url_api');
 
 # Load the google url api library when the spark is loaded
-$autoload['libraries'] = array('google-shortener-api');
+$autoload['libraries'] = array('google_url_api');
