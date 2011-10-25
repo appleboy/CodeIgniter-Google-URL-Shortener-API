8c94a9e27a3635d284b78d14d07eaa71b07c0f5a
Bo-Yi Wu <appleboy.tw@gmail.com>
2011-10-25 20:25:55 +0800

    remove autoload config file setting

--- config/autoload.php
+++ config/autoload.php
@@ -1,7 +1,4 @@
 <?php
 
-# Load the google url api config when the spark is loaded
-$autoload['config'] = array('google_url_api');
-
 # Load the google url api library when the spark is loaded
 $autoload['libraries'] = array('google_url_api');
